c886dbf2d86f355f9df25bc10dbdb0a44f58ae6f
Michael Montgomery <mem22695@gmail.com>
2021-09-27 20:56:38 -0400

    Worked on player_profile

--- ChessCLI/main.py
+++ ChessCLI/main.py
@@ -1,22 +1,8 @@
 from utilities import WrongValueError
-from player_profile import Player_Profile
+from player_profile import PlayerProfile
 if __name__ == '__main__':
-    while True:
-        print(f'Welcome to the Chess.com Analysis CLI!')
-
-        while True:
-            try:
-                choice = int(input(f'1.Get Opening Move Preferences\n'))
-                if choice not in [1]:
-                    raise WrongValueError
-            except WrongValueError:
-                print(f'That was not a choice! Try again! \n')
-                continue
-            else:
-                break
-
-        if choice == 1:
-            player = Player_Profile(input(f'Enter the username: '))
-            player.get_moves()
+    print()
+    player = PlayerProfile('DarthBraves')
+    print(player.get_game_archive())
 
 
--- ChessCLI/player_profile.py
+++ ChessCLI/player_profile.py
@@ -1,6 +1,8 @@
+import chessdotcom.types
 from chessdotcom import *
 
-class Player_Profile:
+
+class PlayerProfile:
     def __init__(self, username):
         self.username = username
         try:
@@ -8,9 +10,24 @@ class Player_Profile:
         except ChessDotComError:
             print(f'There was an error retrieving the user by that username')
 
-
     def get_moves(self):
         try:
             moves = get_player_games_by_month_pgn(self.username, 2021, 9)
+            return moves.__dict__
         except ChessDotComError:
-            print(f'There was an error retrieving ')
+            print(f'There was an error retrieving {self.username} previous games')
+
+    def get_current(self):
+        try:
+            games = get_player_current_games(self.username)
+            return games.__dict__
+        except ChessDotComError:
+            print(f'There was an issue retrieving {self.username} current games')
+
+    def get_game_archive(self):
+        try:
+            games = get_player_game_archives(self.username)
+
+            return games.__dict__
+        except ChessDotComError:
+            print(f'There was an issue retrieving {self.username} archived games')
