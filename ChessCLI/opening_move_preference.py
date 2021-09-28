31cf017bbc61867e7f6e4594b2a6b9d2613672f7
Michael Montgomery <mem22695@gmail.com>
2021-09-28 17:35:02 -0400

    Made it to where we don't call the api everytime when we want to get the games for the year

---
+++ ChessCLI/opening_move_preference.py
@@ -0,0 +1,6 @@
+from player_profile import PlayerProfile
+
+
+
+def opening_move_preference():
+    print()
--- ChessCLI/player_profile.py
+++ ChessCLI/player_profile.py
@@ -6,19 +6,25 @@ from chessdotcom import *
 class PlayerProfile:
     def __init__(self, username):
         self.username = username
+        self.has_games_for_year = False
+        self.games_for_year = []
         try:
             self.profile = get_player_profile(username).json
         except ChessDotComError:
             print(f'There was an error retrieving the user by that username')
 
     def get_moves_for_year(self):
-        try:
-            moves = []
-            for month in range(1, date.today().month):
-                moves.append(get_player_games_by_month_pgn(self.username, date.today().year, month).__dict__)
-            return moves
-        except ChessDotComError:
-            print(f'There was an error retrieving {self.username} previous games')
+        """Probably don't call this many times to reduce number of calls on the Chess.com API"""
+        if not self.has_games_for_year:
+            try:
+
+                for month in range(1, date.today().month):
+                    self.games_for_year.append(get_player_games_by_month_pgn(self.username, date.today().year, month).__dict__)
+                self.has_games_for_year = True
+                return self.games_for_year
+            except ChessDotComError:
+                print(f'There was an error retrieving {self.username} previous games')
+        return self.games_for_year
 
     def get_current(self):
         try:
