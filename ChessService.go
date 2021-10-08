eedc0575a0585914f2357d4e4081cfd45814b97a
Michael Montgomery <mem22695@gmail.com>
2021-10-07 21:28:20 -0400

    worked on ChessService

--- ChessService.go
+++ ChessService.go
@@ -26,3 +26,60 @@ func getPlayerProfile(username string) {
 	fmt.Print(string(body))
 
 }
+
+func getPlayerStats(username string) {
+
+	var url string = " https://api.chess.com/pub/player/" + username + "/stats"
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(string(body))
+}
+
+func isPlayerOnline(username string) {
+
+	var url string = " https://api.chess.com/pub/player/" + username + "/is-online"
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(string(body))
+
+}
+
+func getCurrentDailyGames(username string) {
+	var url string = " https://api.chess.com/pub/player/" + username + "/games"
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(string(body))
+}
