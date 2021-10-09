f1267e2aaa3e0ee25fc70b605fde74e0abdd1417
Michael Montgomery <mem22695@gmail.com>
2021-10-09 18:05:15 -0400

    worked on cli functionality -> added opening move command

--- main.go
+++ main.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Print(getPlayerProfile("DarthBraves"))
+	// fmt.Print(getPlayerProfile("DarthBraves"))
+
+	openingMoveCommand := flag.NewFlagSet("opening", flag.ExitOnError)
+
+	openingGames := openingMoveCommand.String("username", "", "user's chess.com name")
+
+	// see if enough arguments have been passed
+	if len(os.Args) < 2 {
+		fmt.Println("subcommand required")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "opening":
+		openingMoveCommand.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if openingMoveCommand.Parsed() {
+		if *openingGames == "" {
+			openingMoveCommand.PrintDefaults()
+			os.Exit(1)
+		}
+
+		fmt.Print(*openingGames)
+
+	}
 
 }
