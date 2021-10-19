0c982b31e86af2ea3b9927a4f59b0b2b09447cfe
Michael Montgomery <mem22695@gmail.com>
2021-10-19 18:12:54 -0400

    Fixed CLI functionality for opening command

--- main.go
+++ main.go
@@ -3,40 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
-	// fmt.Print(getPlayerProfile("DarthBraves"))
 
-	openingMoveCommand := flag.NewFlagSet("opening", flag.ExitOnError)
+	var openingMoveCommand string
+	flag.StringVar(&openingMoveCommand, "opening", "", "Pass user's chess.com profile name. Default is \"\"")
 
-	openingGames := openingMoveCommand.String("games", "", "Pass user's chess.com name to get their past games opening moves")
+	flag.Parse()
 
 	// see if enough arguments have been passed
 	if len(os.Args) < 2 {
-		log.Println("subcommand required")
+		fmt.Println("subcommand required")
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
-	case "opening":
-		openingMoveCommand.Parse(os.Args[2:])
+	case "-opening":
+		fmt.Print(getOpeningMovePreference(openingMoveCommand))
 	default:
 		flag.PrintDefaults()
 		os.Exit(1)
-	}
-
-	if openingMoveCommand.Parsed() {
-		if *openingGames == "" {
-			openingMoveCommand.PrintDefaults()
-			os.Exit(1)
-		}
-		// logic for -games
-
-		fmt.Print(getOpeningMovePreference(*openingGames))
-
 	}
 
 }
